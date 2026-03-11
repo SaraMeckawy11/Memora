58fe3338e9dc730dbe82d2202885b715ed6e09fa
Salma Meckawy Hemdan <salma.yasser2002a@gmail.com>
2026-03-11 23:23:19 +0200

    update database

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Use 'experimental-edge' runtime if you plan to use edge functions, otherwise default to nodejs
+  // runtime: 'edge', 
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
